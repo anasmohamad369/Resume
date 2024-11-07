6ab5a96fb4efdf3e10793cf9502affba790187ea
ShashankRala <ralashashankyadav0402@gmail.com>
2024-11-07 18:31:02 +0530

    done

--- resume/src/App.jsx
+++ resume/src/App.jsx
@@ -7,9 +7,11 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1> resume app</h1>
-    </>
+    [
+
+      <h1> resume app</h1>,
+      <p>let's start</p>
+    ]
   )
 }
 
