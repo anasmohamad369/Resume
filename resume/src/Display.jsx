1804f856edd8926bd024447c86f8987692175ac4
anas <anasmohamad369@gmail.com>
2024-11-12 13:46:38 +0530

    commmiting the dynamic input fields

--- resume/src/Display.jsx
+++ resume/src/Display.jsx
@@ -1,21 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import Side from './preview/Side';
 import { Form } from './Resumeedit/Form';
-import { useState } from 'react';
-import './Display.css'
+import './Display.css';
+
 export const Display = () => {
-    const [name, setName] = useState('');
-    const [lname, setLname]= useState('');
+  const [name, setName] = useState('');
+  const [lname, setLname] = useState('');
+  const [number,setNumber] = useState('');
+  const [email,setEmail] = useState('');
+  // console.log(email);
+  
+  
+  useEffect(() => {
+    const savedData = JSON.parse(localStorage.getItem('userData'));
+    if (savedData) {
+      setName(savedData.firstName || '');
+      setLname(savedData.lastName || '');
+      setNumber(savedData.number || '');
+      setEmail(savedData.email || "");
+    }
+  }, []);
+
   return (
-       <>
-       <div className="left">
-       <Form name={name} setName={setName}  lname={lname} setLname={setLname}/>
-       </div> 
-    <div className="right">
-    <Side name={name}  lname={lname}/>  
-
-    </div>
-
-       </>
-  )
-}
+    <>
+      <div className="left">
+        <Form Fname={name} setName={setName} lname={lname} setLname={setLname} number={number} setNumber={setNumber} email={email} setEmail={setEmail} />
+      </div>
+      <div className="right">
+        <Side name={name} lname={lname}  number={number} email={email} />
+      </div>
+    </>
+  );
+};
